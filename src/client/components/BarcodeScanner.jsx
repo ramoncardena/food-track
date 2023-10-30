934f67caeb8c57c97f645fab72fe021b7a49f987
Ramon Cardena <ramon.cardena@gmail.com>
2023-10-30 11:27:19 +0100

    Base to use https in a future added. Loading indicator when loading scanner added.

--- src/client/components/BarcodeScanner.jsx
+++ src/client/components/BarcodeScanner.jsx
@@ -1,25 +1,47 @@
 import { useState } from "react";
 
 import { useZxing } from "react-zxing";
+import { BarLoader } from "react-spinners";
 
 import Box from "@mui/material/Box";
 
 // Resources
 import frameImage from "../assets/frame.png";
+import { Stack, Typography } from "@mui/material";
 
 function BarcodeScanner({ onScanned }) {
-  const [result, setResult] = useState("");
+  const [loaded, setLoaded] = useState(false);
+
   const { ref } = useZxing({
     onDecodeResult(result) {
       console.log("RESULT: ", result);
-      setResult(result.getText());
       if (typeof onScanned === "function") onScanned(result.getText());
     },
   });
 
   return (
-    <Box>
-      <video style={{ width: "300px" }} ref={ref} />
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <video
+        style={{ maxWidth: "250px" }}
+        ref={ref}
+        onPlay={() => setLoaded(true)}
+      />
+      {!loaded && (
+        <Stack alignItems="center">
+          <Typography variant="caption" color="primary">
+            Loading Scanner
+          </Typography>
+
+          <BarLoader color="#7fc2d8" />
+        </Stack>
+      )}
     </Box>
   );
 }
